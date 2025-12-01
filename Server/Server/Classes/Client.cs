01c0ddcecc22e4251324a551524e6e93e8a1be15
Mars4213123 <your_email@example.com>
2025-12-01 20:42:04 +0500

    Разработанный класс Client. Кантуганов

--- Server/Server/Classes/Client.cs
+++ Server/Server/Classes/Client.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Server.Classes
 {
     public class Client
     {
+        public string Token { get; set; }
+        public DateTime DateConnect { get; set; }
+
+        public Client()
+        {
+            Random random = new Random();
+            string chars = "QWERTYUIOPASDFGHJKLZXCVBWMqwertyuiopasdfghjklzxcvbmm0123456789";
+
+            Token = new string(Enumerable.Repeat(chars, 15)
+                .Select(x => x[random.Next(chars.Length)]).ToArray());
+            DateConnect = DateTime.Now;
+        }
     }
 }
