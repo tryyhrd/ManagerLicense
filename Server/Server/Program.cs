0cdb03801f3439a4f2aa549d8913c285571d9710
Mars4213123 <your_email@example.com>
2025-12-01 20:53:12 +0500

    Созданный проект, разработанный класс Client. Кантуганов

---
+++ Server/Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace Server
+{
+    class Program
+    {
+        static IPAddress ServerIPAddress;
+        static int ServerPort;
+        static int MaxClient;
+        static int Duration;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
